db61aa8455d780a56c0430a1a8cfdff277373b51
wookyoungkim <viliketh1s98@naver.com>
2020-12-07 15:08:31 +0900

    마크 클릭시 오버레이 띄우기 + 다른데 클릭시 오버레이 지우기 close #11

--- GongGongI/map/views.py
+++ GongGongI/map/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 import json
 
 def showattractions(request):
+    #with와 json 모듈을 이용해 Json파일 불러오기
     with open('static/json/attractions.json', encoding='utf-8') as json_file:
         attractions = json.load(json_file)['response']['body']['items']['item']
 
     attractiondict = []
+    #불러온 json 객체들 중 필요한 데이터만 뽑기
     for attraction in attractions:
         if attraction.get('mapx'):
             content = {
@@ -14,6 +16,10 @@ def showattractions(request):
                 "mapy": str(attraction['mapy']),
                 "addr1": str(attraction['addr1']),
             }
+            if attraction.get('tel'):
+                content['tel'] = str(attraction['tel'])
+            else:
+                content['tel'] = ''
             attractiondict.append(content)
     attractionJson = json.dumps(attractiondict, ensure_ascii=False)
     return render(request, 'map/map.html', {'attractionJson': attractionJson})
