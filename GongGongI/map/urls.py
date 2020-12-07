3cb2ee7d02dc4f2d7017f971d3b8379067bf9d39
wookyoungkim <viliketh1s98@naver.com>
2020-12-07 09:39:42 +0900

    view에서 파싱한 데이터 html에서 넘겨주고 지도에 핀 찍기 close #9 close #10

--- GongGongI/map/urls.py
+++ GongGongI/map/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.showmap),
+    path('', views.showattractions),
 
 ]
--- GongGongI/map/views.py
+++ GongGongI/map/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 import json
 
-def showmap(request):
-    # with open('static/json/attractions.json', encoding='utf-8') as json_file:
-    #     attractions = json.load(json_file)['response']['body']['items']['item']
-    #
-    # attractiondict = []
-    # for attraction in attractions:
-    #     if attraction.get('mapx'):
-    #         content = {
-    #             "title": attraction['title'],
-    #             "mapx": str(attraction['mapx']),
-    #             "mapy": str(attraction['mapy'])
-    #         }
-    #         attractiondict.append(content)
-    # attractionJson = json.dumps(attractiondict, ensure_ascii=False)
-    return render(request, 'map/map.html')#, {'attractionJson': attractionJson})
+def showattractions(request):
+    with open('static/json/attractions.json', encoding='utf-8') as json_file:
+        attractions = json.load(json_file)['response']['body']['items']['item']
+
+    attractiondict = []
+    for attraction in attractions:
+        if attraction.get('mapx'):
+            content = {
+                "title": attraction['title'],
+                "mapx": str(attraction['mapx']),
+                "mapy": str(attraction['mapy'])
+            }
+            attractiondict.append(content)
+    attractionJson = json.dumps(attractiondict, ensure_ascii=False)
+    return render(request, 'map/map.html', {'attractionJson': attractionJson})
 
 
 # Create your views here.
