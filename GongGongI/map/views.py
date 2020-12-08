c0640e15b323be114dc757ed99dc4cdc45464050
wookyoungkim <viliketh1s98@naver.com>
2020-12-09 03:52:55 +0900

    map.html 리팩토링 및 안심식당 Json 파싱

--- GongGongI/map/views.py
+++ GongGongI/map/views.py
@@ -21,8 +21,40 @@ def showattractions(request):
             else:
                 content['tel'] = ''
             attractiondict.append(content)
+
+    with open('static/json/safe_restaurant.json', encoding='utf-8') as json_file:
+        safe_restaurants = json.load(json_file)['data']
+
+    restaurantdict = []
+    #불러온 json 객체들 중 필요한 데이터만 뽑기
+    for restaurant in safe_restaurants:
+        content = {
+            "title": restaurant['사업장명'],
+            "tel": str(restaurant['전화번호']),
+            "menu": str(restaurant['업종상세']),
+            "addr": str(restaurant['주소']),
+        }
+    restaurantdict.append(content)
+    print(restaurantdict)
+
+    with open('static/json/good_price.json', encoding='utf-8') as json_file:
+        good_prices = json.load(json_file)['data']
+
+
+    with open('static/json/markets.json', encoding='utf-8') as json_file:
+        markets = json.load(json_file)['data']
+
+
     attractionJson = json.dumps(attractiondict, ensure_ascii=False)
-    return render(request, 'map/map.html', {'attractionJson': attractionJson})
+    saferestaurantJson = json.dumps(restaurantdict, ensure_ascii=False)
+    goodpriceJson = json.dumps(good_prices, ensure_ascii=False)
+    marketsJson = json.dumps(markets, ensure_ascii=False)
+
+    return render(request, 'map/map.html', {'attractionJson': attractionJson, 'saferestaurantJson': saferestaurantJson,
+                                            'goodpriceJson': goodpriceJson, 'marketsJson': marketsJson})
+
+
+
 
 
 # Create your views here.
